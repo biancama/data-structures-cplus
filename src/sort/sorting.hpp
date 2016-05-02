2aa2fe2cb08ca4169f72f318f17543ac5646a7b5
Massimo Biancalani <massimo.biancalani@gmail.com>
2016-05-02 23:19:47 +0200

    Added bubble sort algorithm

--- src/sort/sorting.hpp
+++ src/sort/sorting.hpp
@@ -6,7 +6,7 @@
 #define DATASTRUCTURED_SORTING_H
 namespace structure {
     template <typename T>
-    void insertionSort(T* array, int n) {
+    void insertionSort(T* array, unsigned int n) {
         for (unsigned int i = 1; i < n; ++i) {
             unsigned int j = i;
             T curr = array[j];
@@ -16,6 +16,43 @@ namespace structure {
             }
             array[j] = curr;
         }
+    }
+
+    template <typename T>
+    void swap(T* array, unsigned int i, unsigned int j) {
+        T app = array[i];
+        array[i] = array[j];
+        array[j] = app;
+    }
+
+    template <typename T>
+    void selectionSort(T* array, unsigned int n) {
+        for (unsigned int i = 0; i < n; ++i) {
+            unsigned int min_index = i;
+            for (int j = i; j < n; ++j) {
+                if (array[j] < array[min_index]) {
+                    min_index = j;
+                }
+            }
+            swap(array, i, min_index);
+        }
+    }
+
+    template <typename T>
+    void bubbleSort(T* array, unsigned int n) {
+        unsigned k = n;
+        bool swapped = true;
+        while (swapped) {
+            swapped = false;
+            for (int i = 0; i < k - 1 ; ++i) {
+                if (array[i+1] < array[i]) {
+                    swapped = true;
+                    swap(array, i , i+1);
+                }
+            }
+            k--;
+        }
+
     }
 }
 #endif //DATASTRUCTURED_SORTING_H
--- test/sort/sorting_test.cpp
+++ test/sort/sorting_test.cpp
@@ -4,11 +4,61 @@
 
 #include "gtest/gtest.h"
 #include "sorting.hpp"
-TEST(sorting_check, sort01) {
+TEST(sorting_check, sortInsertion01) {
     int unsorted_array[8] = {8, 7, 6, 5, 4, 3, 2, 1};
     int expected_array[8] = {1, 2, 3, 4, 5, 6, 7, 8};
     structure::insertionSort(unsorted_array, 8);
     for (int idx=0; idx< 8 ; idx++) {
+        SCOPED_TRACE(idx); //write to the console in which iteration the error occurred
+        ASSERT_EQ(unsorted_array[idx],expected_array[idx]);
+    }
+}
+
+TEST(sorting_check, sortInsertion02) {
+    int unsorted_array[13] = {3, 8, 5, 7, 5, 3, 6, 5, 4, 3, 2, 1, 5};
+    int expected_array[13] = {1, 2, 3, 3, 3, 4, 5, 5, 5, 5, 6, 7, 8};
+    structure::insertionSort(unsorted_array, 13);
+    for (int idx=0; idx< 13 ; idx++) {
+        SCOPED_TRACE(idx); //write to the console in which iteration the error occurred
+        ASSERT_EQ(unsorted_array[idx],expected_array[idx]);
+    }
+}
+
+TEST(sorting_check, sortSelection01) {
+    int unsorted_array[8] = {8, 7, 6, 5, 4, 3, 2, 1};
+    int expected_array[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    structure::selectionSort(unsorted_array, 8);
+    for (int idx=0; idx< 8 ; idx++) {
+        SCOPED_TRACE(idx); //write to the console in which iteration the error occurred
+        ASSERT_EQ(unsorted_array[idx],expected_array[idx]);
+    }
+}
+
+TEST(sorting_check, sortSelection02) {
+    int unsorted_array[13] = {3, 8, 5, 7, 5, 3, 6, 5, 4, 3, 2, 1, 5};
+    int expected_array[13] = {1, 2, 3, 3, 3, 4, 5, 5, 5, 5, 6, 7, 8};
+    structure::selectionSort(unsorted_array, 13);
+    for (int idx=0; idx< 13 ; idx++) {
+        SCOPED_TRACE(idx); //write to the console in which iteration the error occurred
+        ASSERT_EQ(unsorted_array[idx],expected_array[idx]);
+    }
+}
+
+TEST(sorting_check, bubbleSelection01) {
+    int unsorted_array[8] = {8, 7, 6, 5, 4, 3, 2, 1};
+    int expected_array[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    structure::bubbleSort(unsorted_array, 8);
+    for (int idx=0; idx< 8 ; idx++) {
+        SCOPED_TRACE(idx); //write to the console in which iteration the error occurred
+        ASSERT_EQ(unsorted_array[idx],expected_array[idx]);
+    }
+}
+
+TEST(sorting_check, bubbleSelection02) {
+    int unsorted_array[13] = {3, 8, 5, 7, 5, 3, 6, 5, 4, 3, 2, 1, 5};
+    int expected_array[13] = {1, 2, 3, 3, 3, 4, 5, 5, 5, 5, 6, 7, 8};
+    structure::bubbleSort(unsorted_array, 13);
+    for (int idx=0; idx< 13 ; idx++) {
         SCOPED_TRACE(idx); //write to the console in which iteration the error occurred
         ASSERT_EQ(unsorted_array[idx],expected_array[idx]);
     }
